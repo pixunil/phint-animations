6fa1a6aef4753e9848cce6f3da67efcd1029301c
pixunil <pixunil@gmx.de>
2015-09-30 21:59:15 +0200

    Wrapped polius drawing logic into classes

--- drawElements.py
+++ drawElements.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 import cairo
+import math
 
 class Background:
-    grey = 0
-
     def __init__(self):
         self.gradient = cairo.RadialGradient(0, 0, 0, 0, 0, 1)
         self.gradient.add_color_stop_rgb(0, 1, 1, 1)
@@ -14,3 +13,43 @@ class Background:
         ctx.source = self.gradient
         ctx.rectangle(-ctx.width, -ctx.height, 2 * ctx.width, 2 * ctx.height)
         ctx.fill()
+
+class Line:
+    def __init__(self):
+        self.points = []
+
+    def addPoint(self, x, y):
+        self.points.append((x, y))
+
+    def draw(self, ctx):
+        ctx.moveTo(*self.points[0])
+
+        for point in self.points[1:]:
+            ctx.lineTo(*point)
+
+        ctx.stroke()
+
+class Arc:
+    def __init__(self, x, y, radius, start = 0, end = math.pi * 2):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.start = start
+        self.end = end
+
+    def draw(self, ctx):
+        ctx.arc(self.x, self.y, self.radius, self.start, self.end)
+        ctx.stroke()
+
+class Shape:
+    color = None
+
+    def __init__(self):
+        self.shapes = []
+
+    def draw(self, ctx):
+        if self.color:
+            ctx.source = self.color
+
+        for shape in self.shapes:
+            shape.draw(ctx)
--- graphics/polius.py
+++ graphics/polius.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python3
 
 from math import *
+import drawElements
 
 class Shape:
-    def draw(self, ctx):
-        ctx.source = (0, 0, 1)
+    color = (0, 0, 1)
+
+class Line(drawElements.Line):
+    def __init__(self, angle):
+        drawElements.Line.__init__(self)
+
+        self.addPoint(
+            .8 * cos(radians(angle)) - .25 * cos(radians(angle - 30)),
+            .8 * sin(radians(angle)) - .25 * sin(radians(angle - 30))
+        )
+
+        self.addPoint(
+            .8 * cos(radians(angle + 120)) - .25 * cos(radians(angle + 150)),
+            .8 * sin(radians(angle + 120)) - .25 * sin(radians(angle + 150))
+        )
+
+class Arc(drawElements.Arc):
+    def __init__(self, angle):
+        x = .8 * cos(radians(angle))
+        y = .8 * sin(radians(angle))
+
+        start = radians(angle - 230)
+        end = radians(angle - 130)
+
+        drawElements.Arc.__init__(self, x, y, .25, start, end)
+
+class Shape(drawElements.Shape):
+    color = (0, 0, 1)
+
+    def __init__(self):
+        drawElements.Shape.__init__(self)
 
         for angle in range(90, 360, 120):
-            x = .8 * cos(radians(angle))
-            y = .8 * sin(radians(angle))
-
-            start = radians(angle - 230)
-            end = radians(angle - 130)
-
-            ctx.arc(x, y, .25, start, end)
-
-            x -= .25 * cos(radians(angle - 30))
-            y -= .25 * sin(radians(angle - 30))
-            ctx.moveTo(x, y)
-
-            x = .8 * cos(radians(angle + 120)) - .25 * cos(radians(angle + 150))
-            y = .8 * sin(radians(angle + 120)) - .25 * sin(radians(angle + 150))
-            ctx.lineTo(x, y)
-
-            ctx.stroke()
+            self.shapes.append(Line(angle))
+            self.shapes.append(Arc(angle))
